180291cb5b04a0d90be31cdd800e02d3f2c26309
Mattis <draemat11@schueler.bwv-ac.de>
2026-02-13 12:52:35 +0100

    Reapply "first_monster_image"
    This reverts commit 4876b72067c1b3ead4769bd7320802f03acc5462.

--- cursed-crip/menu_button.gd
+++ cursed-crip/menu_button.gd
@@ -1,17 +1,22 @@
 extends Button
 
+@onready var overlay = $Overlay
 
-func _ready() -> void:
-	pass 
+func _ready():
+	overlay.self_modulate = Color(0, 0, 0, 0.18)
 
-func _on_mouse_entered() -> void:
-	scale = Vector2(1.05, 1.05)
+func _on_mouse_entered():
+	scale = Vector2(1.03, 1.03)
+	overlay.self_modulate.a = 0.28
 
-func _on_mouse_exited() -> void:
+func _on_mouse_exited():
 	scale = Vector2(1, 1)
+	overlay.self_modulate.a = 0.18
 
-func _on_button_down() -> void:
+func _on_button_down():
 	scale = Vector2(0.97, 0.97)
+	overlay.self_modulate.a = 0.35
 
-func _on_button_up() -> void:
-	scale = Vector2(1.05, 1.05)
+func _on_button_up():
+	scale = Vector2(1.03, 1.03)
+	overlay.self_modulate.a = 0.28
