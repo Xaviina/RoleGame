8aed46d65f9a7526bc8229bf0cefc8f9723bbd30
Melissa <jansmel11@schueler.bwv-ac.de>
2026-02-18 19:23:11 +0100

    new level
    - added level transition
    - made some oranizantional changes

--- cursed-crip/00_Globals/global_level_manager.gd
+++ cursed-crip/00_Globals/global_level_manager.gd
@@ -1,8 +1,52 @@
 extends Node
 
-var current_tilemap_bounds : Array[Vector2]
-signal TileMapBoundsChanged (bounds:Array[Vector2])
+signal level_load_started
+signal level_loaded
+signal tilemap_bounds_changed (bounds:Array[Vector2])
 
-func ChangeTileMapBounds(bounds:Array[Vector2]) -> void:
+var current_tilemap_bounds : Array[Vector2]
+var target_transition : String
+var position_offset : Vector2
+
+var transition_locked := false
+var transition_lock_time := 1.5   # Sekunden (einstellbar)
+
+func change_tilemap_bounds(bounds:Array[Vector2]) -> void:
 	current_tilemap_bounds = bounds
-	TileMapBoundsChanged.emit(bounds)
+	tilemap_bounds_changed.emit(bounds)
+
+
+func load_new_level(
+		level_path: String,
+		_target_transition : String,
+		_posotion_offset: Vector2
+) -> void:
+	
+	if transition_locked:
+		return
+		
+	transition_locked = true
+	
+	get_tree().paused = true
+	target_transition = _target_transition
+	position_offset = _posotion_offset
+	
+	level_load_started.emit()
+	
+	await get_tree().process_frame # level transition
+	
+	get_tree().change_scene_to_file(level_path)
+	
+	await get_tree().process_frame # level transition
+	
+	get_tree().paused = false
+	
+	await get_tree().process_frame
+	
+	level_loaded.emit()
+	
+	 #  Cooldown starten
+	await get_tree().create_timer(transition_lock_time).timeout
+	transition_locked = false
+	
+	pass
